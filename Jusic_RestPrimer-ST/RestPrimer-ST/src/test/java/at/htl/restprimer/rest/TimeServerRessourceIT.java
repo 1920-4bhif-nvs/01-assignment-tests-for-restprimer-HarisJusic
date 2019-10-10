040d96e214cc9ae5214a037d5c8a7c9fc432e695
Haris Jusic <harci.99@hotmail.com>
2019-10-10 12:59:53 +0200

    fixed errors

--- Jusic_RestPrimer-ST/RestPrimer-ST/src/test/java/at/htl/restprimer/rest/TimeServerRessourceIT.java
+++ Jusic_RestPrimer-ST/RestPrimer-ST/src/test/java/at/htl/restprimer/rest/TimeServerRessourceIT.java
@@ -21,7 +21,7 @@ public class TimeServerRessourceIT {
     @BeforeEach
     public void initClient() {
         this.client = ClientBuilder.newClient();
-        this.tut = this.client.target("http://localhost:8081/restprimer/rs/time");
+        this.tut = this.client.target("http://localhost:8080/restprimer/rs/time");
     }
 
     @Test
